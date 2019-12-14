e679660d4bf840d9ac3e4ee22884b1fea40fe6c0
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-12-14 11:30:53 +0100

    Object Destructuring * Forma antigua de utilizar el Object Destructuring - ES5 para bajo.

--- repasojs/js/AppObject.js
+++ repasojs/js/AppObject.js
@@ -2,49 +2,60 @@
  * Objectos en JavaScript
  */
 
- //Objetos literales - Forma antigua de hacer.
- const persona = {
-     nombre : 'juan',
-     profesion : 'Desarrollador Web',
-     edad: 500
+//Objetos literales - Forma antigua de hacer.
+const persona = {
+  nombre: "juan",
+  profesion: "Desarrollador Web",
+  edad: 500
+};
 
- }
+console.log(persona);
+console.log(persona.nombre);
 
- console.log(persona);
- console.log(persona.nombre);
+//Object Constructor
+function Tarea(nombre, urgencia) {
+  this.nombre = nombre;
+  this.urgencia = urgencia;
+}
 
- //Object Constructor
- function Tarea(nombre, urgencia){
-     this.nombre = nombre;
-     this.urgencia = urgencia;
- }
+//Instanciando un objeto del tipo Tarea,
+const tarea1 = new Tarea("Welton", "Urgente");
 
- //Instanciando un objeto del tipo Tarea,
- const tarea1 = new Tarea('Welton', 'Urgente');
+//Imprimiendo en consola el objeto tarea1.
+console.log(tarea1);
 
- //Imprimiendo en consola el objeto tarea1.
- console.log(tarea1);
-
- /**
-  * Utilizando el Prototype
-  * Son como los métodos de clase en Java.
-  */
+/**
+ * Utilizando el Prototype
+ * Son como los métodos de clase en Java.
+ */
 
 //Creando una clase
-function Empleado(nombre,direccion){
-    this.nombre = nombre;
-    this.direccion = direccion;
+function Empleado(nombre, direccion) {
+  this.nombre = nombre;
+  this.direccion = direccion;
 }
 //Aplicando un método al objeto Empleado
-Empleado.prototype.mostrarInformacion = function(){
-    return `Nombre: ${this.nombre}\nDirección: ${this.direccion}`;
-}
+Empleado.prototype.mostrarInformacion = function() {
+  return `Nombre: ${this.nombre}\nDirección: ${this.direccion}`;
+};
 
-let objeto_Empleado = new Empleado('Welton', 'Calle Juarez');
+let objeto_Empleado = new Empleado("Welton", "Calle Juarez");
 console.log(objeto_Empleado);
 console.log(objeto_Empleado.mostrarInformacion());
 
 /**
- * object Destructuring 
+ * Object Destructuring
  */
- 
+
+//Forma antigua de hacer Object Destructuring
+const aprendiendoJS = {
+  version: {
+    nueva: "ES6",
+    anterior: "ES5"
+  },
+  frameworks: ["React", "VueJS", "AngularJS"]
+};
+
+console.log(aprendiendoJS); //Forma actual de acceder al objecto
+console.log(aprendiendoJS.version.nueva);//Forma antigua de acceder al objeto
+console.log(aprendiendoJS.frameworks[1]);
