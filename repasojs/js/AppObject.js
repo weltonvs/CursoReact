c454c0e239fda33c584c9fbf9cc93afc124afd50
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-12-14 10:50:09 +0100

    Objectos Literales - Forma antigua de hacer.

---
+++ repasojs/js/AppObject.js
@@ -0,0 +1,15 @@
+/**
+ * Objectos en JavaScript
+ */
+
+ //Objetos literales - Forma antigua de hacer.
+ const persona = {
+     nombre : 'juan',
+     profesion : 'Desarrollador Web',
+     edad: 500
+
+ }
+
+ console.log(persona);
+ console.log(persona.nombre);
+ 
