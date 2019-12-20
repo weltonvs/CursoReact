5163932a3116ddce344ba6b01a49930e93a69a9a
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-12-20 14:30:11 +0100

    Crear clase y instanciar objetos de esa clase. * Básico

--- repasojs/js/Clases.js
+++ repasojs/js/Clases.js
@@ -3,3 +3,19 @@
  */
 
 // Clases en JavaScript
+class Tarea{
+
+    constructor(nombre, priodidad){
+        this.nombre = nombre;
+        this.priodidad = priodidad;
+    }
+
+}
+
+//Instanciar un objeto de la clase Tarea
+let objeto1 = new Tarea("Estudiar","Alta");
+let objeto2 = new Tarea("Comprar Coche Nuevo","Alta");
+let objeto3 = new Tarea("Ver series y peliculas","Baja");
+console.log(objeto1);
+console.log(objeto2);
+console.log(objeto3);
